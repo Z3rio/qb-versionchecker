fa6a579d0cfb11a62a8e215d0687e16ad963936a
Zerio <54480523+Z3rio@users.noreply.github.com>
2022-11-22 22:06:01 +0100

    Add checking for outdated resources

---
+++ Models/Resource.cs
@@ -0,0 +1,19 @@
+﻿namespace VersionChecker.Models
+{
+    public class ResourceModel
+    {
+        public class Versions
+        {
+            public string? local { get; set; }
+            public string? current { get; set; }
+        }
+
+        public class Resource
+        {
+            public string? name { get; set; }
+
+            public string? currentVersion { get; set; }
+            public Versions? versions { get; set; }
+        }
+    }
+}
---
+++ Utils/Sorting.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static VersionChecker.Models.ResourceModel;
+
+namespace VersionChecker.Utils
+{
+    internal class Sorting
+    {
+        public static List<Resource> SortResources(List<Resource> resources)
+        {
+            return resources;
+        }
+    }
+}
