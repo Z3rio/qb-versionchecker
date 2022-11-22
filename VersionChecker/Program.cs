e96439475ed8d9c541e19dc14fd53ca1a921b5a7
Zerio <54480523+Z3rio@users.noreply.github.com>
2022-11-22 17:24:32 +0100

    Initialize C# project

---
+++ VersionChecker/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace VersionChecker
+{
+    public static class Program {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        } 
+    }
+}
