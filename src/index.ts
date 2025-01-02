c74b08b65e3f9a1d2aa126608ccae0eb2182ebf4
Zerio <neoboij@gmail.com>
2025-01-03 00:06:31 +0100

    feat: setup simple logic

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,50 @@
 #!/usr/bin/env node
 
+import { input } from "@inquirer/prompts";
+import { Dirent, existsSync, readdirSync } from "fs";
+import path from "path";
+import picocolors from "picocolors";
+
+// Handle inquirer errors
+process.on("uncaughtException", error => {
+  if (!(error instanceof Error) || error.name !== "ExitPromptError") {
+    throw error;
+  }
+});
+
+function getDirectories(currentPath: string, list: Dirent[] = []): Dirent[] {
+  const directories = readdirSync(currentPath, { withFileTypes: true });
+
+  for (const directory of directories) {
+    if (directory.isDirectory()) {
+      if (directory.name.startsWith("qb-")) {
+        list.push(directory);
+      } else {
+        list = getDirectories(path.join(currentPath, directory.name), list);
+      }
+    }
+  }
+
+  return list;
+}
+
 async function main() {
-  console.log("hello");
+  const resourcesPath = await input({
+    message: "Input resources folder path"
+  });
+
+  if (!existsSync(resourcesPath)) {
+    console.warn(picocolors.red("Resources folder not found"));
+    process.exit(1);
+  }
+
+  console.info(picocolors.green("Resources folder found"));
+  console.info("\n");
+
+  const qbDirectories = await getDirectories(resourcesPath);
+  void qbDirectories;
+
+  console.info("\n");
 }
 
 main();
